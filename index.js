f7bf261dcbfe55dda7b4c144d7a38d3c1f0043e6
Steph Heins <stephanie_heins@yahoo.com>
2021-11-04 21:31:58 -0700

    Added dependencies and mysql connect/initialize code to index

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Dependencies 
+const inquirer = require("inquirer")
+const mysql = require("mysql")
+const cTable = require('console.table');
 
+// MySQL Connection Info 
+// Change credentials per user 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeetrackerDB"
+  });
 
-//MySQL Connection 
-
+// MySQL Initialize 
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startApp();
+});
 
 // MENU Prompt 
 
