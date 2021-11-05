164be25e6184254e56bce233e2ec9423aa4b4e15
Steph Heins <stephanie_heins@yahoo.com>
2021-11-04 20:58:39 -0700

    Added role content to seed

--- db/seed.sql
+++ db/seed.sql
@@ -11,6 +11,19 @@ INSERT INTO department (name)
 VALUES ("IT");
 
 -- Role
-
+INSERT INTO role (title, salary, department_id)
+VALUE ("Support Agent", 50000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Support Agent", 65000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Human Resources", 65000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Social Network Specialist", 60000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Strategist", 75000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("IT Support", 70000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Security Analyst", 90000, 4);
 
 -- Employee 
