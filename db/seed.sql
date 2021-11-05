4633ee484a2e850a4337e6db7be8a3e49b167765
Steph Heins <stephanie_heins@yahoo.com>
2021-11-04 20:49:06 -0700

    Added department content to seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,14 @@
--- Department 
+USE employeetrackerDB;
 
+-- Department 
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("HR");
+INSERT INTO department (name)
+VALUES ("Marketing");
+INSERT INTO department (name)
+VALUES ("IT");
 
 -- Role
 
